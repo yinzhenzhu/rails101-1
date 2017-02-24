a7570736c336d1a8cb36baafb153f8a22b7b66cb
yinzhenzhu <304347328@qq.com>
2017-02-25 00:04:45 +0800

    implement group#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,10 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+  
+  def show
+    @group = Group.find(params[:id])
+  end
 
   def new
     @group = Group.new
@@ -17,6 +21,6 @@ class GroupsController < ApplicationController
 
   def group_params
     params.require(:group).permit(:title, :description)
-  end 
+  end
 
 end
